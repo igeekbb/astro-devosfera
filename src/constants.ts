5dc19792e29e8b6505e2ebc94106d82abf0b1531
Geek <154116268+igeekbb@users.noreply.github.com>
2026-02-23 13:40:19 +0800

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,71 +1,70 @@
-import type { Props } from "astro";
-import IconMail from "@/assets/icons/IconMail.svg";
-import IconGitHub from "@/assets/icons/IconGitHub.svg";
-import IconBrandX from "@/assets/icons/IconBrandX.svg";
-import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
-import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
-import IconFacebook from "@/assets/icons/IconFacebook.svg";
-import IconTelegram from "@/assets/icons/IconTelegram.svg";
-import IconPinterest from "@/assets/icons/IconPinterest.svg";
-import { SITE } from "@/config";
-
-interface Social {
-  name: string;
-  href: string;
-  linkTitle: string;
-  icon: (_props: Props) => Element;
-}
-
-export const SOCIALS: Social[] = [
-  {
-    name: "GitHub",
-    href: "https://github.com/0xdres/",
-    linkTitle: `${SITE.title} en GitHub`,
-    icon: IconGitHub,
-  },
-  {
-    name: "X",
-    href: "https://x.com/geekbb",
-    linkTitle: `${SITE.title} en X`,
-    icon: IconBrandX,
-  },
-] as const;
-
-export const SHARE_LINKS: Social[] = [
-  {
-    name: "WhatsApp",
-    href: "https://wa.me/?text=",
-    linkTitle: `Envia este post por WhatsApp`,
-    icon: IconWhatsapp,
-  },
-  {
-    name: "Facebook",
-    href: "https://www.facebook.com/sharer.php?u=",
-    linkTitle: `Comparte este post en Facebook`,
-    icon: IconFacebook,
-  },
-  {
-    name: "X",
-    href: "https://x.com/intent/post?url=",
-    linkTitle: `Comparte este post en X`，
-    icon: IconBrandX,
-  },
-  {
-    name: "Telegram",
-    href: "https://t.me/share/url?url=",
-    linkTitle: `Comparte este post por Telegram`,
-    icon: IconTelegram,
-  },
-  {
-    name: "Pinterest",
-    href: "https://pinterest.com/pin/create/button/?url=",
-    linkTitle: `Comparte este post en Pinterest`,
-    icon: IconPinterest,
-  },
-  {
-    name: "Mail",
-    href: "mailto:?subject=See%20this%20post&body=",
-    linkTitle: `Envia este post por correo`,
-    icon: IconMail,
-  },
+import type { Props } from "astro";  
+import IconMail from "@/assets/icons/IconMail.svg";  
+import IconGitHub from "@/assets/icons/IconGitHub.svg";  
+import IconBrandX from "@/assets/icons/IconBrandX.svg";  
+import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";  
+import IconFacebook from "@/assets/icons/IconFacebook.svg";  
+import IconTelegram from "@/assets/icons/IconTelegram.svg";  
+import IconPinterest from "@/assets/icons/IconPinterest.svg";  
+import { SITE } from "@/config";  
+  
+interface Social {  
+  name: string;  
+  href: string;  
+  linkTitle: string;  
+  icon: (_props: Props) => Element;  
+}  
+  
+export const SOCIALS: Social[] = [  
+  {  
+    name: "GitHub",  
+    href: "https://github.com/0xdres/",  
+    linkTitle: `${SITE.title} en GitHub`,  
+    icon: IconGitHub,  
+  },  
+  {  
+    name: "X",  
+    href: "https://x.com/geekbb",  
+    linkTitle: `${SITE.title} en X`,  
+    icon: IconBrandX,  
+  },  
+] as const;  
+  
+export const SHARE_LINKS: Social[] = [  
+  {  
+    name: "WhatsApp",  
+    href: "https://wa.me/?text=",  
+    linkTitle: `Envia este post por WhatsApp`,  
+    icon: IconWhatsapp,  
+  },  
+  {  
+    name: "Facebook",  
+    href: "https://www.facebook.com/sharer.php?u=",  
+    linkTitle: `Comparte este post en Facebook`,  
+    icon: IconFacebook,  
+  },  
+  {  
+    name: "X",  
+    href: "https://x.com/intent/post?url=",  
+    linkTitle: `Comparte este post en X`,  
+    icon: IconBrandX,  
+  },  
+  {  
+    name: "Telegram",  
+    href: "https://t.me/share/url?url=",  
+    linkTitle: `Comparte este post por Telegram`,  
+    icon: IconTelegram,  
+  },  
+  {  
+    name: "Pinterest",  
+    href: "https://pinterest.com/pin/create/button/?url=",  
+    linkTitle: `Comparte este post en Pinterest`,  
+    icon: IconPinterest,  
+  },  
+  {  
+    name: "Mail",  
+    href: "mailto:?subject=See%20this%20post&body=",  
+    linkTitle: `Envia este post por correo`,  
+    icon: IconMail,  
+  },  
 ] as const;
